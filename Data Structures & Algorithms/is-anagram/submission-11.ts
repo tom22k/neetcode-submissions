9557de967b2ad52d5157021da77aa50c47afaf38
tom22k <143504320+tom22k@users.noreply.github.com>
2026-08-17 17:47:56 +0200

    Add: is-anagram - submission-11

---
+++ Data Structures & Algorithms/is-anagram/submission-11.ts
@@ -0,0 +1,14 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {string} t
+     * @return {boolean}
+     */
+    isAnagram(s: string, t: string): boolean {
+        if (s.length != t.length) {
+            return false;
+        }
+        
+        return s.split("").sort().join("") === t.split("").sort().join("");
+    }
+}
