e494279c92e3601b0daa47f12a28a6ec48837622
tom22k <143504320+tom22k@users.noreply.github.com>
2026-08-17 15:39:45 +0200

    Add: is-palindrome - submission-0

---
+++ Data Structures & Algorithms/is-palindrome/submission-0.ts
@@ -0,0 +1,11 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+    isPalindrome(s: string): boolean {
+        const onlyAlphaNumeric = s.toLowerCase().replaceAll(/[^a-z0-9]/g, "");
+        const reversed = onlyAlphaNumeric.split("").reverse().join("");
+        return onlyAlphaNumeric === reversed;
+    }
+}
