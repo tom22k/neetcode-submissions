1f2228146b1d912b26b26ccdd80266c3fdb9ab2a
tom22k <143504320+tom22k@users.noreply.github.com>
2026-08-17 15:31:03 +0200

    Add: dynamicArray - submission-1

---
+++ Data Structures & Algorithms/dynamicArray/submission-1.ts
@@ -0,0 +1,83 @@
+class DynamicArray {
+    private capacity: number;
+    private size: number;
+    private array: number[];
+
+    /**
+     * @constructor
+     * @param {number} capacity
+     */
+    constructor(capacity: number) {
+        this.capacity = capacity;
+        this.size = 0;
+        this.array = new Array(capacity);
+    }
+
+    /**
+     * @param {number} i
+     * @returns {number}
+     */
+    get(i: number): number {
+        return this.array[i];
+    }
+
+    /**
+     * @param {number} i
+     * @param {number} n
+     * @returns {void}
+     */
+    set(i: number, n: number): void {
+        this.array[i] = n;
+    }
+
+    /**
+     * @param {number} n
+     * @returns {void}
+     */
+    pushback(n: number): void {
+        if (this.size === this.capacity) {
+            this.resize();
+        }
+
+        this.array[this.size] = n;
+        this.size++;
+    }
+
+    /**
+     * @returns {number}
+     */
+    popback(): number {
+        if (this.size > 0) {
+            this.size--;
+        }
+
+        return this.array[this.size];
+    }
+
+    /**
+     * @returns {void}
+     */
+    resize(): void {
+        this.capacity = this.capacity * 2;
+        let newArray = new Array(this.capacity);
+
+        for (let i = 0; i < this.size; i++) {
+            newArray[i] = this.array[i];
+        }
+        this.array = newArray;
+    }
+
+    /**
+     * @returns {number}
+     */
+    getSize(): number {
+        return this.size;
+    }
+
+    /**
+     * @returns {number}
+     */
+    getCapacity(): number {
+        return this.capacity;
+    }
+}
